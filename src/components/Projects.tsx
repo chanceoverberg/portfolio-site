705cd27f50a8edcc83e5436bdba7cec5440a89ab
Chance Overberg <chanceoverberg@gmail.com>
2024-03-12 18:55:40 -0500

    add link to groupoll

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,64 +1,80 @@
 import { ExternalLinkIcon } from "@chakra-ui/icons";
-import { Box, Button, Card, CardBody, CardFooter, CardHeader, Heading, Text, Wrap } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+  Text,
+  Wrap,
+} from "@chakra-ui/react";
 import { FC } from "react";
 
 const Projects: FC = () => {
-    return (
-        <Box pt='3rem' pl='1rem' pr='1rem' h='100vh'>
-            <Wrap justify='center' pt='1rem' pb='30px'>
-                <Card maxW='md'>
-                    <CardHeader>
-                        <Heading size='md'>Groupoll</Heading>
-                    </CardHeader>
-                    <CardBody>
-                        <Text>
-                            A group-based anonymous poll web app built with Next.js. Users can create a group and share
-                                the link with others. Anyone with the link can anonymously add new polls and vote on existing ones.
-                        </Text>
-                    </CardBody>
-                    <CardFooter>
-                        <Button as='a' href='https://github.com/chanceoverberg/groupoll' target='_blank'>
-                            View in GitHub
-                            <ExternalLinkIcon ml='2px' />
-                        </Button>
-                    </CardFooter>
-                </Card>
-                <Card maxW='md'>
-                    <CardHeader>
-                        <Heading size='md'>SpaceTraders Client</Heading>
-                    </CardHeader>
-                    <CardBody>
-                        <Text>
-                            Client app built with React and TypeScript to play SpaceTraders, an API-based multiplayer game.
-                        </Text>
-                    </CardBody>
-                    <CardFooter>
-                        <Button  as='a' href='https://github.com/chanceoverberg/spacetraders' target='_blank'>
-                            View in GitHub
-                            <ExternalLinkIcon ml='2px' />
-                        </Button>
-                    </CardFooter>
-                </Card>
-                <Card maxW='md'>
-                    <CardHeader>
-                        <Heading size='md'>Portfolio Website</Heading>
-                    </CardHeader>
-                    <CardBody>
-                        <Text>
-                            The website you're looking at right now! Built with React and TypeScript to showcase my experience, 
-                            education, and personal projects.
-                        </Text>
-                    </CardBody>
-                    <CardFooter>
-                        <Button as='a' href='https://github.com/chanceoverberg/portfolio-site' target='_blank'>
-                            View in GitHub
-                            <ExternalLinkIcon ml='2px' />
-                        </Button>
-                    </CardFooter>
-                </Card>
-            </Wrap>
-        </Box>
-    )
+  return (
+    <Box pt="3rem" pl="1rem" pr="1rem" h="100vh">
+      <Wrap justify="center" pt="1rem" pb="30px">
+        <Card maxW="md">
+          <CardHeader>
+            <Heading size="md">Groupoll</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              A group-based anonymous poll web app built with Next.js. Users can create a group and
+              share the link with others. Anyone with the link can anonymously add new polls and
+              vote on existing ones.
+            </Text>
+          </CardBody>
+          <CardFooter>
+            <Button as="a" href="https://github.com/chanceoverberg/groupoll" target="_blank">
+              View in GitHub
+              <ExternalLinkIcon ml="2px" />
+            </Button>
+            <Button marginLeft={"10px"} as="a" href="https://www.groupoll.net" target="_blank">
+              Visit Site
+              <ExternalLinkIcon ml="2px" />
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card maxW="md">
+          <CardHeader>
+            <Heading size="md">SpaceTraders Client</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              Client app built with React and TypeScript to play SpaceTraders, an API-based
+              multiplayer game.
+            </Text>
+          </CardBody>
+          <CardFooter>
+            <Button as="a" href="https://github.com/chanceoverberg/spacetraders" target="_blank">
+              View in GitHub
+              <ExternalLinkIcon ml="2px" />
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card maxW="md">
+          <CardHeader>
+            <Heading size="md">Portfolio Website</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              The website you're looking at right now! Built with React and TypeScript to showcase
+              my experience, education, and personal projects.
+            </Text>
+          </CardBody>
+          <CardFooter>
+            <Button as="a" href="https://github.com/chanceoverberg/portfolio-site" target="_blank">
+              View in GitHub
+              <ExternalLinkIcon ml="2px" />
+            </Button>
+          </CardFooter>
+        </Card>
+      </Wrap>
+    </Box>
+  );
 };
 
 export default Projects;
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,61 +1,95 @@
-import { Box, Card, CardBody, CardHeader, ListItem, Stack, Text, UnorderedList, useColorModeValue } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  ListItem,
+  Stack,
+  Text,
+  UnorderedList,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { FC } from "react";
 
 const Work: FC = () => {
-    const bg = useColorModeValue('gray.300', 'gray.700')
-    
-    return (
-        <Box fontSize='3xl' pt='3rem' pl='1rem' h='100vh'>
-            <Stack fontSize='3xl' pr='1rem' pt='1rem' pb='30px'>
-                <Card variant='outline'>
-                    <CardHeader bg={bg} borderRadius='5px 5px 0px 0px'>Epic Systems</CardHeader>
-                    <CardBody pt='10px'>
-                        <Text fontSize='2xl'>Full Stack Engineer | May 2023 - Present</Text>
-                        <Text fontSize='xl'>
-                            Full stack engineer on the Telehealth team with a focus on
-                            our teleconferencing web app, built with React and .NET and used for
-                            over 1.3 million medical video visits each month.
-                        </Text>
-                        <UnorderedList fontSize='xl' pl='10px' pb='15px'>
-                            <ListItem>Lead developer on current project to put our WebRTC vendor implementation behind an 
-                                abstraction layer, reducing our reliance on any given vendor's specific APIs.
-                            </ListItem>
-                            <ListItem>Create new microservices and enhance existing ones.</ListItem>
-                            <ListItem>Develop fixes and enhancements for the front and back end.</ListItem>
-                            <ListItem>Technology: React, Redux, .NET, Azure, CosmosDB, Kubernetes, Docker</ListItem>
-                            <ListItem>Languages: TypeScript, C#, JavaScript</ListItem>
-                        </UnorderedList>
-                        <Text pt='10px' fontSize='2xl'>Quality Manager | July 2019 - May 2023</Text>
-                        <Text fontSize='xl'>
-                            Quality Manager on the Telehealth team with a focus on testing our teleconferencing and patient portal web apps, both built with React and .NET. 
-                            Our patient portal is used by over 160 million patients worldwide.
-                        </Text>
-                        <UnorderedList fontSize='xl' pl='10px'>
-                            <ListItem>Test, design, and assist in the implementation of the most used healthcare software in the US.</ListItem>
-                            <ListItem>Perform automated (C# Selenium scripts) and manual testing.</ListItem>
-                            <ListItem>Manage projects throughout the entire lifecycle, including design, development, usability studies, testing, and release.</ListItem>
-                            <ListItem>Write test plans to efficiently and effectively test as many areas as possible and reduce the risk of bugs and regressions.</ListItem>
-                            <ListItem>Investigate released issues and lead root cause analysis (RCA) meetings and processes.</ListItem>
-                        </UnorderedList>
-                    </CardBody>
-                </Card>
-                <Card variant='outline'>
-                    <CardHeader bg={bg} borderRadius='5px 5px 0px 0px'>Miami University</CardHeader>
-                    <CardBody pt='10px' fontSize='2xl'>
-                        <Text>Assistive Technology Student Associate | Sept 2018 - Jan 2019</Text>
-                        <Text fontSize='xl'>
-                            Provide technical assistance functions, perform administrative duties,
-                            and train others in the use of assistive technology.
-                        </Text>
-                        <UnorderedList fontSize='xl' pl='10px'>
-                            <ListItem>Write, record, and analyze technical documents.</ListItem>
-                            <ListItem>Create alternative text files and closed captioning files for videos.</ListItem>
-                        </UnorderedList>
-                    </CardBody>
-                </Card>
-            </Stack>
-        </Box>
-    )
+  const bg = useColorModeValue("gray.300", "gray.700");
+
+  return (
+    <Box fontSize="3xl" pt="3rem" pl="1rem" h="100vh">
+      <Stack fontSize="3xl" pr="1rem" pt="1rem" pb="30px">
+        <Card variant="outline">
+          <CardHeader bg={bg} borderRadius="5px 5px 0px 0px">
+            Epic Systems
+          </CardHeader>
+          <CardBody pt="10px">
+            <Text fontSize="2xl">Full Stack Engineer | Aug 2022 - Present</Text>
+            <Text fontSize="xl">
+              Full stack engineer on the Telehealth team with a focus on our teleconferencing web
+              app, built with React and .NET and used for over 1.3 million medical video visits each
+              month.
+            </Text>
+            <UnorderedList fontSize="xl" pl="10px" pb="15px">
+              <ListItem>
+                Lead developer on current project to put our WebRTC vendor implementation behind an
+                abstraction layer, reducing our reliance on any given vendor's specific APIs.
+              </ListItem>
+              <ListItem>Create new microservices and enhance existing ones.</ListItem>
+              <ListItem>Develop fixes and enhancements for the front and back end.</ListItem>
+              <ListItem>
+                Technology: React, Redux, .NET, Azure, CosmosDB, Kubernetes, Docker
+              </ListItem>
+              <ListItem>Languages: TypeScript, C#, JavaScript</ListItem>
+            </UnorderedList>
+            <Text pt="10px" fontSize="2xl">
+              Quality Manager | July 2019 - Aug 2022
+            </Text>
+            <Text fontSize="xl">
+              Quality Manager on the Telehealth team with a focus on testing our teleconferencing
+              and patient portal web apps, both built with React and .NET. Our patient portal is
+              used by over 160 million patients worldwide.
+            </Text>
+            <UnorderedList fontSize="xl" pl="10px">
+              <ListItem>
+                Test, design, and assist in the implementation of the most used healthcare software
+                in the US.
+              </ListItem>
+              <ListItem>Perform automated (C# Selenium scripts) and manual testing.</ListItem>
+              <ListItem>
+                Manage projects throughout the entire lifecycle, including design, development,
+                usability studies, testing, and release.
+              </ListItem>
+              <ListItem>
+                Write test plans to efficiently and effectively test as many areas as possible and
+                reduce the risk of bugs and regressions.
+              </ListItem>
+              <ListItem>
+                Investigate released issues and lead root cause analysis (RCA) meetings and
+                processes.
+              </ListItem>
+            </UnorderedList>
+          </CardBody>
+        </Card>
+        <Card variant="outline">
+          <CardHeader bg={bg} borderRadius="5px 5px 0px 0px">
+            Miami University
+          </CardHeader>
+          <CardBody pt="10px" fontSize="2xl">
+            <Text>Assistive Technology Student Associate | Sept 2018 - Jan 2019</Text>
+            <Text fontSize="xl">
+              Provide technical assistance functions, perform administrative duties, and train
+              others in the use of assistive technology.
+            </Text>
+            <UnorderedList fontSize="xl" pl="10px">
+              <ListItem>Write, record, and analyze technical documents.</ListItem>
+              <ListItem>
+                Create alternative text files and closed captioning files for videos.
+              </ListItem>
+            </UnorderedList>
+          </CardBody>
+        </Card>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Work;
